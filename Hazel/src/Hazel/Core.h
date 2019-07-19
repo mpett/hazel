dd58e03992abb1fd6577aa0c4a30dcbe51baf4dc
Martin Pettersson <kontakt@martinpettersson.se>
2019-07-19 09:25:57 +0200

    Entry point setup

---
+++ Hazel/src/Hazel/Core.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#ifdef HZ_BUILD_DLL
+		#define HAZEL_API __declspec(dllexport)
+	#else
+		#define HAZEL_API __declspec(dllexport)
+	#endif
+#else
+	#error Hazel only supports Windows
+#endif
