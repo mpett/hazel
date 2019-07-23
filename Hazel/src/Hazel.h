085d8e9b9075c07d2c7b5b4637f5ffc2032f22be
Martin Pettersson <kontakt@martinpettersson.se>
2019-07-23 09:37:08 +0200

    Premake script

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,8 @@
 // For use by Hazel applications.
 
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
+#include "Hazel/Log.cpp"
 
 //----Entry Point---------------------------
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #ifdef HZ_PLATFORM_WINDOWS
-
+#include "Log.h"
 extern Hazel::Application* Hazel::CreateApplication();
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized log!");
+	int a = 5;
+	HZ_INFO("test Var={0}", a);
+	
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
